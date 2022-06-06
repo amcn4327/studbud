a1c3087a9f8eebfe05fef7d8b7b91447c289d6b8
amcn4327 <amcn4327@uni.sydney.edu.au>
2022-06-06 16:36:06 +1000

    Kanban board added

---
+++ public/kanscript.js
@@ -0,0 +1,19 @@
+$('.drag-item').on('dragstart', function(event) {
+    event.originalEvent.dataTransfer.setData('listItem', $(this).index())
+  })
+  
+  $('.kanban-column-done').on('drop', function(event) {
+    console.log('in the drop function');
+    event.preventDefault();
+    let listItemIndex = event.originalEvent.dataTransfer.getData('listItem')
+    $(this).append($('.drag-item').eq(listItemIndex))
+  })
+    .on('dragover', function(event) {
+    event.preventDefault()
+  })
+    .on('dragenter', function(event) {
+    $(this).addClass('drop-zone-active')
+  })
+    .on('dragleave', function(event) {
+    $(this).removeClass('drop-zone-active')      
+  })
