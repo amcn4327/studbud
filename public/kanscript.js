146c8157f4e2c38dbf6d024d000a3168dc9af944
amcn4327 <amcn4327@uni.sydney.edu.au>
2022-06-07 11:05:52 +1000

    Trying to add another column to kanban board

--- public/kanscript.js
+++ public/kanscript.js
@@ -2,6 +2,23 @@ $('.drag-item').on('dragstart', function(event) {
     event.originalEvent.dataTransfer.setData('listItem', $(this).index())
   })
   
+  $('.kanban-column-progress').on('drop', function(event) {
+    console.log('in the drop function');
+    event.preventDefault();
+    let listItemIndex = event.originalEvent.dataTransfer.getData('listItem')
+    $(this).append($('.drag-item').eq(listItemIndex))
+  })
+
+  .on('dragover', function(event) {
+    event.preventDefault()
+  })
+    .on('dragenter', function(event) {
+    $(this).addClass('drop-zone-active')
+  })
+    .on('dragleave', function(event) {
+    $(this).removeClass('drop-zone-active')      
+  })
+
   $('.kanban-column-done').on('drop', function(event) {
     console.log('in the drop function');
     event.preventDefault();
